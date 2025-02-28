9e1f86c0a8071b39023ad08f1cd57ab206ce2083
MHJ51214 <mhasnainjafri51214@gmail.com>
2025-02-28 17:34:21 +0500

    Update breadcrumb image URL logic to use route slug

--- resources/views/templates/basic/partials/breadcrumb.blade.php
+++ resources/views/templates/basic/partials/breadcrumb.blade.php
@@ -3,10 +3,16 @@
     $showBreadcrumb   = $showBreadcrumb??true;
     $routes           = ['user.register', 'influencer.register', 'user.login', 'influencer.login', 'password.request'];
     $currentRouteName = request()->route()->getName();
+    $uri=request()->route('slug');
+    if(!$uri){
+        $uri = request()->route()->uri();
+    }
+    // dd($uri);
 @endphp
 
 @if(!in_array($currentRouteName, $routes))
-    <section class="inner-banner bg_img" style="background: url('{{ getImage('assets/images/frontend/breadcrumb/'.@$breadcrumb->data_values->image,'1920x250') }}') center;">
+
+    <section class="inner-banner bg_img" style="background: url('{{ getImage('assets/images/frontend/breadcrumb/'.$uri.'.png','1920x250') }}') center;">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-7 col-xl-6 text-center">
